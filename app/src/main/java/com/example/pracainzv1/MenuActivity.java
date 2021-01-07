e2d24b4ffce28fdc01192d8dd82f0d36fe038808
Cezary Prusinowski <cezary.prusinowski@gmail.com>
2021-01-07 20:46:30 +0100

    Menu layout

--- app/src/main/java/com/example/pracainzv1/MenuActivity.java
+++ app/src/main/java/com/example/pracainzv1/MenuActivity.java
@@ -2,12 +2,45 @@ package com.example.pracainzv1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.pracainzv1.databinding.ActivityMenuBinding;
 
 public class MenuActivity extends Activity {
+
+    private ActivityMenuBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        binding = ActivityMenuBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.btnMenuHide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MenuActivity.this, "Hide", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        binding.btnMenuUnhide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MenuActivity.this, "Unhide", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        binding.btnMenuStat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MenuActivity.this, "Stat", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/pracainzv1/WelcomeActivity.java
+++ app/src/main/java/com/example/pracainzv1/WelcomeActivity.java
@@ -1,13 +1,34 @@
 package com.example.pracainzv1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.pracainzv1.databinding.ActivityWelcomeBinding;
 
 public class WelcomeActivity extends Activity {
+
+    private ActivityWelcomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        binding = ActivityWelcomeBinding.inflate(getLayoutInflater());
+
+        View view = binding.getRoot();
+        setContentView(view);
+
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.btnWelcomeStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, MenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
