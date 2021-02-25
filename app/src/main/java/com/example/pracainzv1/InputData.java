f0b148f92cba609e031e048d5ce76f18a7eae7e6
Cezary Prusinowski <cezary.prusinowski@gmail.com>
2021-02-25 19:07:43 +0100

    Kodowanie i odkodowywanie wiadomości w pliku jpg.

--- app/src/main/java/com/example/pracainzv1/InputData.java
+++ app/src/main/java/com/example/pracainzv1/InputData.java
@@ -3,6 +3,7 @@ package com.example.pracainzv1;
 import android.util.Log;
 
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.BitSet;
 
 public class InputData {
@@ -20,9 +21,18 @@ public class InputData {
     public String getMessageFromFile(){
 
         int first = findFirstFlagIndex();
-        Log.v("Pos", String.valueOf(first));
+        if (first==0)
+            return "";
+        int second = findSecondFlagIndex(first);
+//        Log.v("Pos", String.valueOf(first));
+//        Log.v("Pos", String.valueOf(second));
 
-        return "";
+        String message = findMessageInFile(first,second);
+//        Log.v("Mess", message);
+
+
+
+        return message;
     }
 
     private int findFirstFlagIndex(){
@@ -54,7 +64,47 @@ public class InputData {
         return 0;
     }
 
-    private int findSecondFlagIndex(){
+    private int findSecondFlagIndex(int firstFlagIndex){
+        boolean result = false;
+        byte[] bytes = new byte[32];
+        inputContainerDataByteBuffer.position(firstFlagIndex).limit(inputContainerDataByteBuffer.capacity());
+
+        while(!result){
+            BitSet bytesLSBBitSet = new BitSet();
+            inputContainerDataByteBuffer.get(bytes);
+
+
+            for (int i=0; i < bytes.length; i++){
+                if (((bytes[i]) & 1) == 1)
+                    bytesLSBBitSet.set(i);
+            }
+
+            if ((inputContainerDataByteBuffer.position() == inputContainerDataByteBuffer.capacity())
+                    || inputContainerDataByteBuffer.position() > inputContainerDataByteBuffer.capacity()-bytes.length) {
+                break;
+            }
+
+            if (bytesLSBBitSet.equals(flagBitSet)) {
+                result = true;
+                return inputContainerDataByteBuffer.position()-32;
+            }
+
+            inputContainerDataByteBuffer.position(inputContainerDataByteBuffer.position()-31);
+        }
         return 0;
+    }
+
+    private String findMessageInFile(int first, int second){
+        inputContainerDataByteBuffer.position(first).limit(second);
+        byte[] bytes = new byte[inputContainerDataByteBuffer.limit()-inputContainerDataByteBuffer.position()];
+        inputContainerDataByteBuffer.get(bytes);
+        BitSet bitSet = new BitSet();
+
+        for (int i=0; i<bytes.length; i++){
+            if (((bytes[i]) & 1) == 1)
+                bitSet.set(i);
+        }
+        byte[] messageBytes = bitSet.toByteArray();
+        return new String(messageBytes);
     }
 }
